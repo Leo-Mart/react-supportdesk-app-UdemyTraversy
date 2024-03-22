0f212aabc0960912ab2f274c17cd94c099fc233b
Martin Leo <martin.christoffer.leo@gmail.com>
2024-03-22 21:12:52 +0100

    continued working through the course, frontend and backend is working for both login and register new user. Next up is creating tickets

---
+++ backend/controllers/ticketController.js
@@ -0,0 +1,57 @@
+const asyncHandler = require('express-async-handler');
+
+const User = require('../models/userModel');
+const Ticket = require('../models/ticketModel');
+
+// @desc   get user tickets
+// @route  GET /api/tickets
+// @acess  private
+
+const getTickets = asyncHandler(async (req, res) => {
+  // Get user using the id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const tickets = await Ticket.find({ user: req.user.id });
+
+  res.status(200).json(tickets);
+});
+
+// @desc   create new ticket
+// @route  POST /api/tickets
+// @acess  private
+
+const createTicket = asyncHandler(async (req, res) => {
+  const { product, description } = req.body;
+
+  if (!product || !description) {
+    res.status(400);
+    throw new Error('please add a product and description');
+  }
+
+  const user = await User.findById(req.user.id);
+
+  // Get user using the id in the JWT
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status: 'new',
+  });
+
+  res.status(201).json(ticket);
+});
+
+module.exports = {
+  getTickets,
+  createTicket,
+};
